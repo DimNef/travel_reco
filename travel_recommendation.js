86a69ac5360c0110d48016e194444f1f61417aea
DimNef <dimkrakis1@gmail.com>
2024-12-01 08:19:07 -0500

    search dictionary levenshtein console

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -27,6 +27,43 @@ function submitContactForm() {
 function clearContactFormInputs() {
     document.getElementById('fname').value = '';
     document.getElementById('lname').value = '';
-    document.getElementById('country').value = 'Australia';
+    document.getElementById('country').value = '';
     document.getElementById('subject').value = '';
 }
+
+/*search functionality with some tolerance */
+
+const SEARCHABLES = ['beach', 'country', 'city', 'temple', 'australia', 'japan', 'brazil', 'cambodia', 'india', 'polynesia', 'sao paulo', 'copacabana'];
+const MIN_DISTANCE = 3;
+function searchHandler() {
+    const searchText = document.getElementById('keywordInput').value.toLowerCase();
+  const filteredDestinations = SEARCHABLES.filter((destination) => {
+    const distance = levenshtein(destination.toLowerCase(), searchText);
+    console.log(distance);
+    return distance <= MIN_DISTANCE;
+  });
+  console.log(filteredDestinations);
+}
+
+/*levenshteins distance */
+function levenshtein (str1, str2) {
+    const track = Array(str2.length + 1).fill(null).map(() =>
+    Array(str1.length + 1).fill(null));
+    for (let i = 0; i <= str1.length; i += 1) {
+       track[0][i] = i;
+    }
+    for (let j = 0; j <= str2.length; j += 1) {
+       track[j][0] = j;
+    }
+    for (let j = 1; j <= str2.length; j += 1) {
+       for (let i = 1; i <= str1.length; i += 1) {
+          const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
+          track[j][i] = Math.min(
+             track[j][i - 1] + 1, // deletion
+             track[j - 1][i] + 1, // insertion
+             track[j - 1][i - 1] + indicator, // substitution
+          );
+       }
+    }
+    return track[str2.length][str1.length];
+ };
